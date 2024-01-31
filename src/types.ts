6ccacd9dce4dd51661751871c5d3db12ad76f093
kenkomu <komukenneth216@gmail.com>
2024-01-31 12:38:23 +0300

    worked on th types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,4 @@
-// export const PAIRS:string='ETH/USD'| []
-export const PAIRS: string[] = ['ETH/USD'];
+
+// Example: Generating pairs programmatically
+const currencies = ['ETH', 'BTC', 'LTC', 'XRP'];
+export const PAIRS: string[] = currencies.map(currency => `${currency}/USD`);
